be7dc7b1a9f28b982a372bdf0ac9973a22fa4007
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-07-11 23:37:23 +0700

    fix db conn

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: 38046,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 })
